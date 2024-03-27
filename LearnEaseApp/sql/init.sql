7594ec8d3f203d6fd858ac48efd1641d1b5126d9
makedoniaz <jafar.sadig04@gmail.com>
2024-03-27 21:20:16 +0400

    Add init.sql file

---
+++ LearnEaseApp/sql/init.sql
@@ -0,0 +1,32 @@
+create database LearnEase
+
+use LearnEase
+
+create table Courses (
+	[Id] int primary key identity,
+	[Name] nvarchar(100) not null,
+	[Description] nvarchar(500) not null,
+	AmountOfLectures int,
+	CreationDate datetime2 not null
+)
+
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Jurrchuko odsyy', 'Ogpelgmhq lzlyn bdwzbpymyfet', 0, '5/26/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Dfaulnyxe zlfwd', 'Pprbrksyg vnauk dvbysgjoaxgv', 0, '10/1/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Rbfddexnr bsqwe', 'Bhpevntog bedrx vyphmgwlxjxh', 0, '1/16/2024');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Oqdmeucio gozjh', 'Mcurtprtt zmqfa phitmyqjgdzf', 0, '2/2/2024');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Skjpablpq dmhph', 'Prcbmiows dxdrr suimlurdsntu', 0, '11/9/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Jmucxzeun cxvqx', 'Usiitujlf shapb jjmaheytmnlq', 0, '10/23/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Tltbyraug qzppv', 'Hybanehcr qiasw veyzliczhvxa', 0, '11/26/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Likxnvckl nziqo', 'Cixixcwtg yczwl uurbuimsedbk', 0, '12/19/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Rigewaonh oblnz', 'Hgisdkyvx hwyzt halmjveihtzp', 0, '7/20/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Darhhdfwp speyt', 'Ffpzaqagt xecnq sgwuxuwdktru', 0, '7/3/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Ujjgxwrbf yjxel', 'Xybyfwwaq eafdd iemdlnbkzvou', 0, '9/19/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Upvaiqoae hgpwl', 'Koiabwnns hqhhz rlmwaxmehmbn', 0, '8/14/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Ilcssllla okmcy', 'Eyjhsines hknvt zcnyonjrpoyk', 0, '4/4/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Icthcixaz mpcyf', 'Vbnishloa ywibj nolhcqjzdwnd', 0, '3/17/2024');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Fkckodmon fktqe', 'Dgwyjqsrv itheu xdwuqupteiyg', 0, '11/24/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Swzjvjyhg roaxl', 'Edubzyqdi gojaj erjsulaejzkr', 0, '9/21/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Cnuowwylu covtt', 'Beyspudgx aztoc gtvarunpxqji', 0, '3/7/2024');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Pbdmsbmsk ddchr', 'Xneozlywz wqfbl iveqlxioyedw', 0, '8/16/2023');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Axmljnarr lpdvt', 'Lvdrvrjck bpqej hhftzkqvqsed', 0, '2/14/2024');
+insert into Courses ([Name], [Description], AmountOfLectures, CreationDate) values ('Tygjtkzvh gapfm', 'Dlmswwoun aypqe pjhkbbuzaqdu', 0, '7/30/2023');
