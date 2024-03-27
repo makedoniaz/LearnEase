a350c9e53d0fd1d07bf21021aca0991ef440a8ee
makedoniaz <jafar.sadig04@gmail.com>
2024-03-27 22:32:16 +0400

    Add CourseDapperRepository and CourseORM

--- LearnEaseApp/ORMs/CourseORM.cs
+++ LearnEaseApp/ORMs/CourseORM.cs
@@ -1,14 +1,10 @@
-namespace LearnEaseApp.Models;
+namespace LearnEaseApp.ORMs;
 
-public class Course
+public class CourseORM
 {
-    public int Id { get; set; }
-
     public string? Name { get; set; }
 
     public string? Description { get; set; }
-
-    public int AmountOfLectures { get; set; }
 
     public DateTime? CreationDate { get; set; }
 }
--- LearnEaseApp/Program.cs
+++ LearnEaseApp/Program.cs
@@ -1,8 +1,12 @@
-﻿namespace LearnEaseApp;
+﻿using LearnEaseApp.Models;
+using LearnEaseApp.Repositories;
+
+namespace LearnEaseApp;
 class Program
 {
     static void Main()
     {
+        const string connectionString = "Server =localhost; Database=LearnEase; TrustServerCertificate=True; Trusted_Connection=True; User Id=admin;Password=admin" ;
         
     }
 }
