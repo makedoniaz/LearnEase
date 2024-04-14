9aed811bca8f79a63e88ee2f248a97db3c1b8169
makedoniaz <jafar.sadig04@gmail.com>
2024-04-14 19:01:43 +0400

    Add Course Model

---
+++ LearnEase/Models/Course.cs
@@ -0,0 +1,15 @@
+namespace LearnEase.Models;
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int AmountOfLectures { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+    }
+}
