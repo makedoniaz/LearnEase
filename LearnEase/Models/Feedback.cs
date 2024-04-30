789f889fcc74c69ce69d03c2c8230d5104d7a836
makedoniaz <jafar.sadig04@gmail.com>
2024-04-30 20:44:23 +0400

    Add Feedback model

---
+++ LearnEase/Models/Feedback.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnEase.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string? Text { get; set; }
+
+        public int Rating { get; set; }
+
+        public int CourseId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
